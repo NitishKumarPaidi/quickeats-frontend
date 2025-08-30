18ddef7fc05de7484f8534d5a9ac267910d1ef42
Nitish Kumar <pydnitish@gmail.com>
2025-08-30 15:09:20 +0530

    changed files again

--- quickeats.model/src/main/java/com/example/quickeats/config/securityConfig.java
+++ quickeats.model/src/main/java/com/example/quickeats/config/securityConfig.java
@@ -6,16 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class securityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // disable CSRF for testing with Postman
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/users/signup").permitAll() // allow signup API without login
-                .anyRequest().authenticated() // other APIs need login
-            );
+                .csrf().disable() // disable CSRF for testing with Postman
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/signup").permitAll() // allow signup API without login
+                        .anyRequest().authenticated() // other APIs need login
+                );
         return http.build();
     }
 }
--- quickeats.model/src/main/java/com/example/quickeats/model/Application.java
+++ quickeats.model/src/main/java/com/example/quickeats/model/Application.java
@@ -3,7 +3,7 @@ package com.example.quickeats.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.quickeats")
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
